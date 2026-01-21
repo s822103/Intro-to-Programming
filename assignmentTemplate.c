ff836555d207f118a218aee8ddcf6023b5826c56
Philip <104602428+s822103@users.noreply.github.com>
2026-01-20 23:09:17 -0600

    First code in CPE 2073

---
+++ assignmentTemplate.c
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////
+//
+// CPE 2073
+//
+// Homework 1
+//
+// I certify that the code below is my own work.
+// Your name and UTSA ID
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif // _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+ int main(){
+    printf("Hello world");
+ }
